d47548bb7adf9cab3219440a60190171fe1d3810
Tonnie <vontonnie@gmail.com>
2022-10-07 08:55:51 +0300

    Add provideUerRepository() dia function into Repository module

--- app/src/main/java/net/simplifiedcoding/di/RepositoryModule.kt
+++ app/src/main/java/net/simplifiedcoding/di/RepositoryModule.kt
@@ -1,10 +1,21 @@
 package net.simplifiedcoding.di
 
+import dagger.Binds
 import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import net.simplifiedcoding.data.repository.UserRepositoryImpl
+import net.simplifiedcoding.domain.repository.UserRepository
+import javax.inject.Singleton
 
+@InstallIn(SingletonComponent::class)
 @Module
+
 abstract class RepositoryModule {
 
-    
+    @Binds
+    @Singleton
+
+    abstract fun provideUserRepository(userRepository: UserRepositoryImpl):UserRepository
 
 }
