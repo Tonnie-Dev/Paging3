1e6abb76d4be82bde4b1eb327ff92447927ceaac
Tonnie <vontonnie@gmail.com>
2022-10-07 20:26:15 +0300

    Add provideUsersAPI di function

--- app/src/main/java/net/simplifiedcoding/di/AppModule.kt
+++ app/src/main/java/net/simplifiedcoding/di/AppModule.kt
@@ -1,5 +1,7 @@
 package net.simplifiedcoding.di
 
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
@@ -7,6 +9,7 @@ import dagger.hilt.components.SingletonComponent
 import net.simplifiedcoding.data.remote.UsersApi
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
 import retrofit2.create
 import javax.inject.Singleton
 
@@ -19,11 +22,11 @@ object AppModule {
     @Singleton
     fun provideUsersAPI(okHttpClient: OkHttpClient): UsersApi {
 
-        val moshi =
+        val moshi = Moshi.Builder().addLast(KotlinJsonAdapterFactory()).build()
 
         return Retrofit.Builder()
                 .baseUrl(UsersApi.BASE_URL)
-                .addConverterFactory()
+                .addConverterFactory(MoshiConverterFactory.create(moshi))
                 .client(okHttpClient)
                 .build()
                 .create()
