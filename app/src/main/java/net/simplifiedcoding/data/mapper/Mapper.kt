6bc3b3a30b3b326c2d898bb633195bcc0e0efb2e
Tonnie <vontonnie@gmail.com>
2022-10-08 21:01:51 +0300

    Add UserDTO.toModel mapper extension function

--- app/src/main/java/net/simplifiedcoding/data/mapper/Mapper.kt
+++ app/src/main/java/net/simplifiedcoding/data/mapper/Mapper.kt
@@ -1,6 +1,8 @@
 package net.simplifiedcoding.data.mapper
 
+import net.simplifiedcoding.data.remote.dto.UserDTO
 import net.simplifiedcoding.data.remote.dto.UsersResponseDTO
+import net.simplifiedcoding.domain.model.User
 import net.simplifiedcoding.domain.model.UsersResponse
 
 fun UsersResponseDTO.toModel(): UsersResponse {
@@ -10,5 +12,17 @@ fun UsersResponseDTO.toModel(): UsersResponse {
             limit = this.limit,
             page = this.page,
             total = this.total
+    )
+}
+
+
+fun UserDTO.toModel(): User {
+
+    return User(
+            firstName = this.firstName,
+            id = this.id,
+            lastName = this.lastName,
+            picture = this.picture,
+            title = this.title
     )
 }
