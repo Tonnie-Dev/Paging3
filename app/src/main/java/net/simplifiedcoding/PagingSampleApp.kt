8e124e7b3da1f7e0b8eaec3616b90b1d3e0bc1c0
Tonnie <vontonnie@gmail.com>
2022-10-11 19:06:04 +0300

    Initialize Timber in the Oncreate function

--- app/src/main/java/net/simplifiedcoding/PagingSampleApp.kt
+++ app/src/main/java/net/simplifiedcoding/PagingSampleApp.kt
@@ -2,6 +2,12 @@ package net.simplifiedcoding
 
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
-class PagingSampleApp: Application()
+class PagingSampleApp: Application(){
+    override fun onCreate() {
+        super.onCreate()
+        Timber.plant(Timber.DebugTree())
+    }
+}
