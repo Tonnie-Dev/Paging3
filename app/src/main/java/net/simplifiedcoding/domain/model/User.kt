9945ed57e9c5525e0563f93cdc97afa2ca932de1
Tonnie <vontonnie@gmail.com>
2022-10-11 20:17:34 +0300

    Capitalize name string for User data class

--- app/src/main/java/net/simplifiedcoding/domain/model/User.kt
+++ app/src/main/java/net/simplifiedcoding/domain/model/User.kt
@@ -1,13 +1,16 @@
 package net.simplifiedcoding.domain.model
 
+import androidx.compose.ui.text.capitalize
+import androidx.compose.ui.text.intl.Locale
+
 data class User(
     val firstName: String,
     val id: String,
     val lastName: String,
     val picture: String,
     val title: String
-){
+) {
 
-    val name:String
-    get() = "$title $firstName $lastName"
+    val name: String
+        get() = "${if (title == "miss") "Miss" else title.capitalize(locale = Locale.current) + "."} $firstName $lastName"
 }
